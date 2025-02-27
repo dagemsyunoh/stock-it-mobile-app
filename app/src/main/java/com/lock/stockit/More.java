5f13fcf2e165f0d2b9c688a33c29fa5bdbd874fa
Lock <81725500+dagemsyunoh@users.noreply.github.com>
2025-02-27 17:01:00 +0800

    user management
    Signed-off-by: Lock <jgfranciaph@gmail.com>

--- app/src/main/java/com/lock/stockit/More.java
+++ app/src/main/java/com/lock/stockit/More.java
@@ -15,25 +15,51 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class More extends Fragment {
     FirebaseAuth auth = FirebaseAuth.getInstance();
-    Button button;
-    TextView textView;
+    public static String change;
+    Button buttonManage, buttonChangeEmail, buttonChangePassword, buttonSignOUt;
     FirebaseUser user = auth.getCurrentUser();
+    TextView emailUser;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_more, container, false);
-        button = view.findViewById(R.id.sign_out_button);
-        textView = view.findViewById(R.id.email_user);
+        buttonManage = view.findViewById(R.id.manage_users);
+        buttonChangeEmail = view.findViewById(R.id.change_email);
+        buttonChangePassword = view.findViewById(R.id.change_password);
+        buttonSignOUt = view.findViewById(R.id.sign_out_button);
+        emailUser = view.findViewById(R.id.email_user);
 
         if (user == null) {
             loader();
         }
         else {
-            textView.setText(user.getEmail());
+            emailUser.setText(user.getEmail());
+        }
+        if (LoaderActivity.admin) {
+            buttonManage.setVisibility(View.VISIBLE);
+        }
+        else {
+            buttonManage.setVisibility(View.GONE);
         }
 
-        button.setOnClickListener(v -> {
+        buttonManage.setOnClickListener(v -> {
+            Intent i = new Intent(getActivity(), ManageUsersActivity.class);
+            startActivity(i);
+        });
+
+        buttonChangeEmail.setOnClickListener(v -> {
+            change = "email";
+            Intent i = new Intent(getActivity(), ChangeActivity.class);
+            startActivity(i);
+        });
+        buttonChangePassword.setOnClickListener(v -> {
+            change = "password";
+            Intent i = new Intent(getActivity(), ChangeActivity.class);
+            startActivity(i);
+        });
+        buttonSignOUt.setOnClickListener(v -> {
             auth.signOut();
             loader();
         });
