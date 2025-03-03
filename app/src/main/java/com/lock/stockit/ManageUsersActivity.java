43a352ad7cce9f1df7ea98937f1fddd54500264f
Lock <jgfranciaph@gmail.com>
2025-03-03 15:40:10 +0800

    user management
    Signed-off-by: Lock <jgfranciaph@gmail.com>

--- app/src/main/java/com/lock/stockit/ManageUsersActivity.java
+++ app/src/main/java/com/lock/stockit/ManageUsersActivity.java
@@ -50,6 +50,7 @@ public class ManageUsersActivity extends AppCompatActivity {
         buttonBack.setOnClickListener(v -> finish());
 
 
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
