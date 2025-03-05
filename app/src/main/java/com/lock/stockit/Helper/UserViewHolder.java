fab270a03d9bde403db74718a79a0facbef64162
Lock <81725500+dagemsyunoh@users.noreply.github.com>
2025-03-05 17:04:17 +0800

    managing admin and active status of users
    Signed-off-by: Lock <jgfranciaph@gmail.com>

--- app/src/main/java/com/lock/stockit/Helper/UserViewHolder.java
+++ app/src/main/java/com/lock/stockit/Helper/UserViewHolder.java
@@ -6,9 +6,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.cardview.widget.CardView;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.lock.stockit.Model.UserModel;
 import com.lock.stockit.R;
 
@@ -19,6 +22,7 @@ public class UserViewHolder extends BaseViewHolder{
     private final SwitchCompat admin, activated;
     private final ImageView rightImage;
     private final CardView cardView;
+    CollectionReference colRef = FirebaseFirestore.getInstance().collection("users");
 
     public UserViewHolder(View itemView, UserListeners customListeners) {
         super(itemView, customListeners);
@@ -46,33 +50,57 @@ public class UserViewHolder extends BaseViewHolder{
         if (swipeState != SwipeState.NONE) {
             rightImage.setOnClickListener(view -> getListener().onClickRight(item, position));
         }
-        cardView.setOnClickListener(view -> LogDebug("on Click Card"));
+
+        admin.setOnClickListener(view -> confirmChange("admin", admin));
+
+        activated.setOnClickListener(view -> confirmChange("activated", activated));
+
+        cardView.setOnClickListener(view -> { }); // Do not remove, it is required for the swipe to work
         //endregion
         //region On Touch Swipe
-        if (swipeState == SwipeState.NONE) {
-            return;
-        }
+        if (swipeState == SwipeState.NONE) return;
         cardView.setOnTouchListener((view, event) -> {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
                     dXLead = view.getX() - event.getRawX();
                     dXTrail = view.getRight() - event.getRawX();
-                    LogDebug("MotionEvent.ACTION_DOWN");
                     return false;
                 case MotionEvent.ACTION_MOVE:
                     view.getParent().requestDisallowInterceptTouchEvent(true);
                     getListener().onRetainSwipe(item, position);
                     onAnimate(view, onSwipeMove(event.getRawX() + dXLead, event.getRawX() + dXTrail,swipeState), 0L);
                     item.setState(getSwipeState(event.getRawX() + dXLead, event.getRawX() + dXTrail, swipeState));
-                    LogDebug("MotionEvent.ACTION_MOVE");
                     return false;
                 case MotionEvent.ACTION_UP:
                     onAnimate(view, onSwipeUp(item.getState()), 250L);
-                    LogDebug("MotionEvent.ACTION_UP");
                     return false;
                 default:
                     return true;
             }
         });
+    }
+
+    private void updateData(String field, SwitchCompat switchClicked) {
+        colRef.whereEqualTo("email", email.getText().toString()).get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) return;
+            colRef.document(task.getResult().getDocuments().get(0).getId()).update(field, switchClicked.isChecked());
+        });
+    }
+
+    private void confirmChange(String field, SwitchCompat switchClicked) {
+        String message;
+        if (switchClicked.isChecked()) message = email.getText().toString() + " will be able to access certain data and functions. Are you sure you want to continue?";
+        else message = email.getText().toString() + " will be unable to access certain data and functions. Are you sure you want to continue?";
+        AlertDialog alertDialog = new AlertDialog.Builder(cardView.getContext())
+                .setIcon(R.drawable.ic_warning)
+                .setTitle("Warning! User status will be changed")
+                .setMessage(message)
+                .setPositiveButton("OK", (dialog, which) -> {
+                    updateData(field, switchClicked);
+                    dialog.dismiss();
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                .create();
+        alertDialog.show();
     }
 }
