0c97dbe9142e09d30014c117667d9c85ec1858a8
Lock <81725500+dagemsyunoh@users.noreply.github.com>
2025-03-26 03:13:44 +0800

    added receipt history
    Signed-off-by: Lock <jgfranciaph@gmail.com>

--- app/src/main/java/com/lock/stockit/HomeFragment.java
+++ app/src/main/java/com/lock/stockit/HomeFragment.java
@@ -5,24 +5,77 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
-    protected String type;
-    protected TextView textView;
+//    private final String type = LoaderActivity.admin ? "an Admin" : "a User";
+    TableRow tableRow;
+
+    TableLayout receiptTable;
     @SuppressLint("SetTextI18n") //remove later
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        textView = view.findViewById(R.id.home_text);
-        if (LoaderActivity.admin) type = "an Admin";
-        else type = "a User";
-        textView.setText("You are " + type);
+
+        receiptTable = view.findViewById(R.id.receipt_history);
+        initializeReceiptHistory();
 
         return view;
+    }
+
+    private void initializeReceiptHistory() {
+        FirebaseFirestore.getInstance().collection("receipts").orderBy("invoice no").get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) return;
+
+            getReceiptTableRow("Invoice #", "Date & Time", "Total", "Amount Rendered\nCash", "Items");
+
+            for (var document : task.getResult()) {
+                StringBuilder items = new StringBuilder();
+                for (var item : (ArrayList<?>) document.get("items"))
+                    items.append(item.toString()).append("\n");
+                getReceiptTableRow(document.getString("invoice no"),
+                        document.getString("date-time"),
+                        "PHP " + document.getDouble("total"),
+                        "PHP " + document.getDouble("amount rendered cash"),
+                        items.toString());
+            }
+        });
+
+    }
+
+    private void getReceiptTableRow(String invoiceNo, String dateTime, String total, String amountRendered, String items) {
+        TableRow tableRow = new TableRow(getActivity());
+        tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+        TextView textView1 = new TextView(getActivity());
+        textView1.setText(invoiceNo);
+        tableRow.addView(textView1);
+
+        TextView textView2 = new TextView(getActivity());
+        textView2.setText(dateTime);
+        tableRow.addView(textView2);
+
+        TextView textView3 = new TextView(getActivity());
+        textView3.setText(total);
+        tableRow.addView(textView3);
+
+        TextView textView4 = new TextView(getActivity());
+        textView4.setText(amountRendered);
+        tableRow.addView(textView4);
+
+        TextView textView5 = new TextView(getActivity());
+        textView5.setText(items);
+        tableRow.addView(textView5);
+
+        receiptTable.addView(tableRow, new  TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
     }
 }
